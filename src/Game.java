a88cb5466db4cb6b87081b3c80e7bf6b573e3faf
Iluytik Andrei <ilyutsik.andrei@gmail.com>
2025-01-23 20:59:12 +0300

    divided parts Game.play method into smaller methods Game.checkValidInput, Game.changePlayerWord, Game.printStatus, Game.setToString

--- src/Game.java
+++ src/Game.java
@@ -1,35 +1,29 @@
 import java.util.*;
 
 public class Game {
+    private static Set<Character> errorChars;
+    private static int errorCounter;
+    private static String secretWord;
+    private static int secretWordLength;
 
     public static void play() {
-        String secretWord = HiddenWordProvider.getWord();
-        int secretWordLength = secretWord.length();
+        secretWord = HiddenWordProvider.getWord();
+        secretWordLength = secretWord.length();
         char[] playerWord = new char[secretWordLength];
         Arrays.fill(playerWord, '_');
 
-        Set<Character> errorChars = new HashSet<>();
-        int errorCounter = 0;
+        errorChars = new HashSet<>();
+        errorCounter = 0;
 
-        printStatus(errorCounter, playerWord, setToString(errorChars));
+        printStatus(playerWord, errorChars);
         while (errorCounter < 6) {
             String input = Main.scanner.next();
             if (checkValidInput(input)) {
                 char letter = input.charAt(0);
 
-                boolean isChange = false;
-                for (int l = 0; l < secretWordLength; l++) {
-                    if (secretWord.charAt(l) == letter) {
-                        playerWord[l] = letter;
-                        isChange = true;
-                    }
-                }
-                if (!isChange && !errorChars.contains(letter)) {
-                    errorCounter++;
-                    errorChars.add(letter);
-                }
+                changePlayerWord(playerWord, letter);
 
-                printStatus(errorCounter, playerWord, setToString(errorChars));
+                printStatus(playerWord, errorChars);
                 if (Arrays.equals(playerWord, secretWord.toCharArray())) {
                     System.out.println("Победа!!!!!");
                     return;
@@ -37,6 +31,20 @@ public class Game {
             }
         }
         System.out.println("Поражение, загаднанное слово --> " + secretWord);
+    }
+
+    private static void changePlayerWord(char[] playerWord, char letter) {
+        boolean isChange = false;
+        for (int l = 0; l < secretWordLength; l++) {
+            if (secretWord.charAt(l) == letter) {
+                playerWord[l] = letter;
+                isChange = true;
+            }
+        }
+        if (!isChange && !errorChars.contains(letter)) {
+            errorCounter++;
+            errorChars.add(letter);
+        }
     }
 
     private static boolean checkValidInput(String input) {
@@ -52,9 +60,9 @@ public class Game {
         return true;
     }
 
-    private static void printStatus(int errorCounter, char[] playerWord, String errorCharsString) {
+    private static void printStatus(char[] playerWord, Set<Character> errorChars) {
         System.out.println(HangmanAsciiPicture.values()[errorCounter].asciiPicture);
-        System.out.println("Количество ошибок : " + errorCounter + "     неподходящие буквы : " + errorCharsString);
+        System.out.println("Количество ошибок : " + errorCounter + "     неподходящие буквы : " + setToString(errorChars));
         System.out.println(playerWord);
     }
 
